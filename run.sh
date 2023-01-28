270a8b9ef9b7334a29e5f4ffc1cb05f9e2d0dbec
Contrebande Labs <60847654+contrebande-labs@users.noreply.github.com>
2023-01-27 22:13:01 -0500

    startup script

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt update
+apt -y upgrade
+npm install -g npm@latest
+
+if [ ! -f "/medusa" ]
+then
+  git clone https://github.com/contrebande-labs/medusa
+  cd medusa
+  npm i
+  npx medusa telemetry --disable
+  npx medusa seed -f ./data/seed.json
+  npx medusa user --email "${ADMIN_USER_EMAIL}" --password "${ADMIN_USER_PASSWORD}"
+  npx medusa start
+else
+  cd medusa
+  git pull
+  npm i
+  npx medusa telemetry --disable
+  npx medusa start
+fi
