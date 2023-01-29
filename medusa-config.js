cbd4425c0bdfe527321a625bcef4d32c6a9d4e70
Contrebande Labs <60847654+contrebande-labs@users.noreply.github.com>
2023-01-29 00:04:37 -0500

    Plugin Cloudflare R2.

--- medusa-config.js
+++ medusa-config.js
@@ -51,14 +51,14 @@ const plugins = [
     },
   },
   {
-    resolve: "medusa-file-s3",
+    resolve: "medusa-file-cloudflare-r2",
     options: {
-      s3_url: process.env.R2_PUBLIC_URL,
+      public_domain: process.env.R2_PUBLIC_URL,
       bucket: process.env.R2_BUCKET_NAME,
       region: "auto",
       access_key_id: process.env.R2_ACCESS_KEY,
       secret_access_key: process.env.R2_SECRET_KEY,
-      endpoint: process.env.R2_S3_ENDPOINT,
+      s3_endpoint: process.env.R2_S3_ENDPOINT,
     },
   },
   {
